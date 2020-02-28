1d78c237185807d88fc24378222008cb501fa654
Kitwang Chan <kitwang.c@gmail.com>
2020-02-28 14:01:05 +0800

    build(rollup.confi.js): add es module bundle

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+/* eslint import/no-extraneous-dependencies: off */
+import path from 'path';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import vue from 'rollup-plugin-vue';
+
+const r = p => path.join(__dirname, '.', p);
+
+module.exports = {
+  input: r('src/index.js'),
+  output: {
+    file: r('dist/vue-loading-skeleton.esm.js'),
+    format: 'cjs'
+  },
+  plugins: [
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
+    vue(),
+    babel({ exclude: r('node_modules/**') })
+  ],
+  external: ['dayjs']
+};
